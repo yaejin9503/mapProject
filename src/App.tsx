aaa4c314cd0067a3c65d62503851986d69b62524
yejin <jimmy0152@naver.com>
2024-01-08 14:06:54 +0900

    - map의 props ts 타입 추가 - document.getElementId -> useRef로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
 import KakaoMap from "./components/common/KakaoMap"
 
-// 테스트 커밋을 위한 수정사항
-function App() {
-  // const [count, setCount] = useState(0)
 
-  //  
+function App() {
   return (
     <>
-      <div>dkdkdkdk</div>
       <KakaoMap latitude={33.450701} longitude={126.570667}/> 
     </>
   )
