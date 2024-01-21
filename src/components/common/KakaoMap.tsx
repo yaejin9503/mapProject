ef365e34a99766045608fcd8a787195859e69673
yejin <jimmy0152@naver.com>
2024-01-21 13:06:07 +0900

    Fix : 렌더링 로직 변경 - 위도 경도 변경 될 때마다 맵 생성까지 다시 되는 로직 분리 -> 위도 경도 변경 되는 로직 , 맵 생성 되는 로직 - map state가 없으면 렌더링 되지 않게

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -2,27 +2,35 @@ import { useEffect, useRef, useState } from "react";
 import { IPropsMap } from "../../commons/types/types";
 import KakaoMapMarker from "../units/KakaoMapMarker";
 
-const { kakao } = window; 
-
 export default function KakaoMap(props: IPropsMap) {
-  const mapDiv = useRef(null);
-  const [ map, setMap ] = useState();
+  const mapDiv = useRef<HTMLDivElement>(null);
+  const [ map, setMap ] = useState<kakao.maps.Map>();
+  const [ containerState, setContainerState ] = useState<HTMLDivElement | null>(null); 
+  const [ optionsState , setOptionsState ] = useState<kakao.maps.MapOptions | null>(null);  
 
   useEffect(() => {
-    const container = mapDiv.current; //지도를 담을 영역의 DOM 레퍼런스
-    const options = { //지도를 생성할 때 필요한 기본 옵션
+    setContainerState(mapDiv.current as HTMLDivElement); 
+    setOptionsState({ //지도를 생성할 때 필요한 기본 옵션
       center: new kakao.maps.LatLng(props.latitude, props.longitude), //지도의 중심좌표.
       level: 7 //지도의 레벨(확대, 축소 정도) 
-    };
-    setMap(new kakao.maps.Map(container, options)); //지도 생성 및 객체 리턴
+    }); 
 
   }, [props.latitude, props.longitude])
 
+  useEffect(() => { 
+    if(containerState != null && optionsState != null){ 
+      // 맵을 실제적으로 생성함 
+     setMap(new kakao.maps.Map(containerState as HTMLDivElement, optionsState as kakao.maps.MapOptions)); //지도 생성 및 객체 리턴
+    }
+  }, [containerState, optionsState])
 
 
   return (
     <>
-      <KakaoMapMarker map={map} data={props.data}/>
+      { 
+        map ? 
+        <KakaoMapMarker map={map} data={props.data}/> : <> </>
+      }
       <div ref={mapDiv}
         style={{
           width: '100vw',
@@ -30,6 +38,5 @@ export default function KakaoMap(props: IPropsMap) {
         }}>
       </div>
     </>
-    
   )
 }
