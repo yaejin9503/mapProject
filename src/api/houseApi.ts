f367a55c5fd4e7712965bcb731480026bbedc7e7
yejin <jimmy0152@naver.com>
2024-02-02 14:11:51 +0900

    Styles: prettier 코드 포맷팅 적용

--- src/api/houseApi.ts
+++ src/api/houseApi.ts
@@ -1,77 +1,85 @@
-import { HouseInfo, AddressFucResult } from "../commons/types/types";
-import _ from "lodash";
+import { HouseInfo, AddressFucResult } from '../commons/types/types'
+import _ from 'lodash'
 
 // 원본 데이터 가져오는 함수
 export const getHouseData = async (): Promise<HouseInfo[]> => {
-  const result = await fetch("./src/api/house.json");
-  const { items } = await result.json();
+    const result = await fetch('./src/api/house.json')
+    const { items } = await result.json()
 
-  return items;
-};
+    return items
+}
 
 // 중복 주소 제거한 데이터 가져오는 함수
 export const uniqHouseData = async () => {
-  const houseData = await getHouseData();
-  const houseArray: Array<HouseInfo> = houseData.map((house: HouseInfo) => {
-    house.notSpaceAddress = house.address
-      .slice(0, house.address.indexOf("("))
-      .replace("서울특별시", "서울")
-      .split(" ")
-      .join("")
-      .trim();
-    return house;
-  });
+    const houseData = await getHouseData()
+    const houseArray: Array<HouseInfo> = houseData.map((house: HouseInfo) => {
+        house.notSpaceAddress = house.address
+            .slice(0, house.address.indexOf('('))
+            .replace('서울특별시', '서울')
+            .split(' ')
+            .join('')
+            .trim()
+        return house
+    })
 
-  const notDupplicate: Array<HouseInfo> = _.uniqBy(
-    houseArray,
-    "notSpaceAddress"
-  );
-  return notDupplicate;
-};
+    const notDupplicate: Array<HouseInfo> = _.uniqBy(
+        houseArray,
+        'notSpaceAddress'
+    )
+    return notDupplicate
+}
 
 // 주소에 맞는 위도, 경도 추가하는 함수
 export const houseWithGeocoder = async () => {
-  const _uniqHouseData: HouseInfo[] = await uniqHouseData();
-  const geocoder = new kakao.maps.services.Geocoder(); // 장소 검색 서비스 객체를 생성한다.
+    const _uniqHouseData: HouseInfo[] = await uniqHouseData()
+    const geocoder = new kakao.maps.services.Geocoder() // 장소 검색 서비스 객체를 생성한다.
 
-  const addressSearchPromise = (
-    house: HouseInfo
-  ): Promise<AddressFucResult> => {
-    return new Promise((resolve) => {
-      geocoder.addressSearch(
-        house.address,
-        (
-          result: Array<AddressFucResult>,
-          status: kakao.maps.services.Status
-        ) => {
-          if (status == "OK" && status === kakao.maps.services.Status.OK) {
-            resolve(result[0]);
-          }
-        }
-      );
-    });
-  };
+    const addressSearchPromise = (
+        house: HouseInfo
+    ): Promise<AddressFucResult> => {
+        return new Promise((resolve) => {
+            geocoder.addressSearch(
+                house.address,
+                (
+                    result: Array<AddressFucResult>,
+                    status: kakao.maps.services.Status
+                ) => {
+                    if (
+                        status == 'OK' &&
+                        status === kakao.maps.services.Status.OK
+                    ) {
+                        resolve(result[0])
+                    }
+                }
+            )
+        })
+    }
 
-  try {
-    const returnData = await Promise.all(
-      _uniqHouseData.map((house) => addressSearchPromise(house))
-    );
-    if (!returnData) return;
+    try {
+        const returnData = await Promise.all(
+            _uniqHouseData.map((house) => addressSearchPromise(house))
+        )
+        if (!returnData) return
 
-    const geocoderData = _uniqHouseData.map((house) => {
-      const findAddress = returnData.filter(
-        (data) =>
-          data.address_name.split(" ").join("").trim() === house.notSpaceAddress
-      );
-      if (findAddress && Array.isArray(findAddress) && findAddress.length > 0) {
-        house.longitude = Number(findAddress[0].x);
-        house.latitude = Number(findAddress[0].y);
-      }
-      return house;
-    });
+        const geocoderData = _uniqHouseData.map((house) => {
+            const findAddress = returnData.filter(
+                (data) =>
+                    data.address_name.split(' ').join('').trim() ===
+                    house.notSpaceAddress
+            )
+            if (
+                findAddress &&
+                Array.isArray(findAddress) &&
+                findAddress.length > 0
+            ) {
+                house.longitude = Number(findAddress[0].x)
+                house.latitude = Number(findAddress[0].y)
+            }
+            return house
+        })
 
-    return geocoderData;
-  } catch (e) {
-    console.error("error message:", e);
-  }
-};
+        return geocoderData
+    } catch (e) {
+        console.error('error message:', e)
+    }
+}
--- src/components/units/KakaoMapMarker.tsx
+++ src/components/units/KakaoMapMarker.tsx
@@ -1,52 +1,80 @@
 import { useEffect, useState } from "react";
+
 import { HouseInfo, MarkerInfo } from "../../commons/types/types";
 import { useOptionStore } from "../../store/optionStore";
+import ModalInfo from "../common/modal/ModalInfo";
 
-interface IMarkerprops { 
-  map: kakao.maps.Map
-  data : Array<HouseInfo> | undefined
+interface IMarkerprops {
+  map: kakao.maps.Map;
+  data: Array<HouseInfo> | undefined;
 }
 
-export default function KakaoMapMarker(props : IMarkerprops){  
-  const [ marker, setMarker ] = useState<Array<HouseInfo> | undefined>([]); 
-  const { rank } = useOptionStore(); 
+export default function KakaoMapMarker(props: IMarkerprops) {
+  const [marker, setMarker] = useState<Array<HouseInfo> | undefined>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { rank } = useOptionStore();
 
-  useEffect(() => { 
+  useEffect(() => {
     return setMarker(props.data);
-  }, [props.data])
+  }, [props.data]);
 
+  const addEventHandle = (
+    target: HTMLElement,
+    type: string,
+    callback: () => void
+  ) => {
+    if (target.addEventListener) {
+      target.addEventListener(type, callback);
+    }
+  };
 
-  useEffect(() => { 
-    const positions: Array<MarkerInfo> = [];
-
+  useEffect(() => {
     props?.data?.forEach((house: HouseInfo) => {
+      const contents = document.createElement("button");
+      contents.style.backgroundColor = "#f8f5f5";
+      contents.style.color = "#000";
+      contents.style.borderRadius = "12px";
+      contents.style.boxShadow = "0 0 0 1px #000 inset";
+      contents.innerHTML = `
+      <div style="text-align:center;
+                  padding: 3px 7px;
+                  font-size: 12px;
+                  ">
+          <div>sh주택공사</div> 
+          <div style="font-size:11px">
+            ${(rank === 1
+              ? house.firstNumYouthRent
+              : house.secondNumYouthRent
+            ).toLocaleString("ko-KR", { style: "currency", currency: "KRW" })}
+          </div>
+      </div>
+      `;
       const obj: MarkerInfo = {
-        content: `<div style="color:#000;text-align:center;background-color: #f8f5f5;padding: 3px 7px;font-size: 12px;border-radius: 12px;box-shadow: 0 0 0 1px #000 inset;">
-                      <div>sh주택공사</div> 
-                      <div style="font-size:11px">${(rank === 1? house.firstNumYouthRent : house.secondNumYouthRent).toLocaleString('ko-KR', {style: 'currency', currency: 'KRW'})}</div>
-                  </div>`,
-        latlng: new kakao.maps.LatLng(house.latitude, house.longitude)
+        content: contents,
+        latlng: new kakao.maps.LatLng(house.latitude, house.longitude),
       };
-      positions.push(obj);
-    })
-
-
-    for (let i = 0; i < positions.length; i++) {
-      // 마커를 생성합니다
 
       const customOverlay = new kakao.maps.CustomOverlay({
-        position: positions[i].latlng,
-        content: positions[i].content   
-      }); 
+        position: obj.latlng,
+        content: obj.content,
+      });
 
       customOverlay.setMap(props.map);
-    }
 
+      addEventHandle(contents, "click", function () {
+        // contents.
+        contents.style.backgroundColor = "#000";
+        contents.style.color = "#f8f5f5";
+        setIsOpen(true);
+      });
+    });
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [marker, rank])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [marker, rank]);
 
-  return( 
-    <></>
-  ); 
+  return (
+    <>
+      <ModalInfo isOpen={isOpen} setIsOpen={() => setIsOpen(!isOpen)} />
+    </>
+  );
 }
