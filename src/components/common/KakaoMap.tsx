3c873cfc4feb1c78f9ccc565b9e2be55568b9078
yejin <jimmy0152@naver.com>
2024-02-02 14:09:37 +0900

    Fix : 맵은 최초 렌더링 시에만 생성되도록 - 중심값 옮기는 코드를 추가하고, 맵이 한번만 생성되도록 변경했음..!

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -4,39 +4,52 @@ import KakaoMapMarker from "../units/KakaoMapMarker";
 
 export default function KakaoMap(props: IPropsMap) {
   const mapDiv = useRef<HTMLDivElement>(null);
-  const [ map, setMap ] = useState<kakao.maps.Map>();
-  const [ containerState, setContainerState ] = useState<HTMLDivElement | null>(null); 
-  const [ optionsState , setOptionsState ] = useState<kakao.maps.MapOptions | null>(null);  
+  const [map, setMap] = useState<kakao.maps.Map>();
+  const [containerState, setContainerState] = useState<HTMLDivElement | null>(
+    null
+  );
+  const [optionsState, setOptionsState] =
+    useState<kakao.maps.MapOptions | null>(null);
 
   useEffect(() => {
-    setContainerState(mapDiv.current as HTMLDivElement); 
-    setOptionsState({ //지도를 생성할 때 필요한 기본 옵션
+    setContainerState(mapDiv.current as HTMLDivElement);
+    setOptionsState({
+      //지도를 생성할 때 필요한 기본 옵션
       center: new kakao.maps.LatLng(props.latitude, props.longitude), //지도의 중심좌표.
-      level: 7 //지도의 레벨(확대, 축소 정도) 
-    }); 
+      level: 7, //지도의 레벨(확대, 축소 정도)
+    });
+  }, []);
 
-  }, [props.latitude, props.longitude])
-
-  useEffect(() => { 
-    if(containerState != null && optionsState != null){ 
-      // 맵을 실제적으로 생성함 
-     setMap(new kakao.maps.Map(containerState as HTMLDivElement, optionsState as kakao.maps.MapOptions)); //지도 생성 및 객체 리턴
+  useEffect(() => {
+    if (containerState != null && optionsState != null) {
+      // 맵을 실제적으로 생성함
+      setMap(
+        new kakao.maps.Map(
+          containerState as HTMLDivElement,
+          optionsState as kakao.maps.MapOptions
+        )
+      ); //지도 생성 및 객체 리턴
     }
-  }, [containerState, optionsState])
+  }, [containerState, optionsState]);
 
+  // 위도 경도 중심으로 지도 중앙값을 변경해줌.
+  useEffect(() => {
+    const moveLatLon = new kakao.maps.LatLng(props.latitude, props.longitude);
+
+    // 지도 중심을 이동 시킵니다
+    map?.panTo(moveLatLon);
+  }, [props.longitude, props.latitude, map]);
 
   return (
     <>
-      { 
-        map ? 
-        <KakaoMapMarker map={map} data={props.data}/> : <> </>
-      }
-      <div ref={mapDiv}
+      {map ? <KakaoMapMarker map={map} data={props.data} /> : <> </>}
+      <div
+        ref={mapDiv}
         style={{
-          width: '100vw',
-          height: '100vh'
-        }}>
-      </div>
+          width: "100vw",
+          height: "100vh",
+        }}
+      ></div>
     </>
-  )
+  );
 }
