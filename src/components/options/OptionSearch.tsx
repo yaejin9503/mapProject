2c579e325823e168fe18b587e3881b81bbb310ab
yejin <jimmy0152@naver.com>
2024-02-02 14:08:25 +0900

    Refactor : 명령형 -> 선언적으로 className 변경 - 불필요한 주석 또한 제거

--- src/components/options/OptionSearch.tsx
+++ src/components/options/OptionSearch.tsx
@@ -1,29 +1,40 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useRef } from "react";
+import { useState } from "react";
 import { useOptionStore } from "../../store/optionStore";
 
-export default function OptionSearch(){ 
+export default function OptionSearch() {
+  const [firstBtnClassName, setFirstBtnClassName] = useState(
+    "text-sm mr-2 py-1 px-2 rounded-[3px] bg-white shadow"
+  );
+  const [secondBtnClassName, setSecondBtnClassName] = useState(
+    "text-sm mr-2 py-1 px-2 rounded-[3px]"
+  );
 
-  const firstBtn  = useRef<HTMLButtonElement>(null); 
-  const secondBtn = useRef<HTMLButtonElement>(null); 
-  const { setRank } = useOptionStore(); 
+  const { setRank } = useOptionStore();
 
-  const onClickRankBtn = ( value : number) =>{ 
-    setRank(value); 
+  const onClickRankBtn = (value: number) => {
+    setRank(value);
 
-    firstBtn.current?.classList.toggle('bg-white');
-    firstBtn.current?.classList.toggle('shadow'); 
-  
-    secondBtn.current?.classList.toggle('bg-white'); 
-    secondBtn.current?.classList.toggle('shadow');
-  }
+    const className = "text-sm mr-2 py-1 px-2 rounded-[3px]";
+    let selected1 = "";
+    let selected2 = "";
 
+    if (value === 1) {
+      selected1 += " bg-white shadow";
+    } else {
+      selected2 += " bg-white shadow";
+    }
+    setFirstBtnClassName(className + selected1);
+    setSecondBtnClassName(className + selected2);
+  };
 
-  return(
+  return (
     <div className="z-10 absolute top-0 right-0 m-10 bg-[#f5f5f5] px-1 py-1 text-black rounded-[3px] border-solid border border-[#9ca3af]">
-      <button onClick={() => onClickRankBtn(1)} ref={firstBtn} className="text-sm mr-2 py-1 px-2 rounded-[3px] bg-white shadow">1순위</button>
-      <button onClick={() => onClickRankBtn(2)} ref={secondBtn} className="text-sm py-1 px-2 rounded-[3px]">2-3순위</button>
+      <button onClick={() => onClickRankBtn(1)} className={firstBtnClassName}>
+        1순위
+      </button>
+      <button onClick={() => onClickRankBtn(2)} className={secondBtnClassName}>
+        2-3순위
+      </button>
     </div>
-  )
-  
+  );
 }
