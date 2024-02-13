408c18832c562e444181bcc96b37e74d227e407a
yejin <jimmy0152@naver.com>
2024-02-13 14:55:18 +0900

    Refactor : 불필요한 state 파생상태로 변경

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -1,44 +1,38 @@
 import { useEffect, useRef, useState } from "react";
 import { IPropsMap } from "../../commons/types/types";
 import KakaoMapMarker from "../units/KakaoMapMarker";
+import { useUserStore } from "../../store/mapStore";
 
 export default function KakaoMap(props: IPropsMap) {
   const mapDiv = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<kakao.maps.Map>();
-  const [containerState, setContainerState] = useState<HTMLDivElement | null>(
-    null
-  );
-  const [optionsState, setOptionsState] =
-    useState<kakao.maps.MapOptions | null>(null);
+  // 왜 필요해? kakao map이 생성되는 타이밍을 알아야된다, 마커에도 중앙값에도,
+  //  effect의 동작은 렌더링이 일어났을 떄, 디펜던시로 일어난 값이 바뀌는 지 알아야 되는 거 니까.
+  const { longitude, latitude } = useUserStore();
 
   useEffect(() => {
-    setContainerState(mapDiv.current as HTMLDivElement);
-    setOptionsState({
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(props.latitude, props.longitude), //지도의 중심좌표.
+    const container = mapDiv.current as HTMLDivElement;
+    const option = {
+      center: new kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표.
       level: 7, //지도의 레벨(확대, 축소 정도)
-    });
-  }, []);
+    };
 
-  useEffect(() => {
-    if (containerState != null && optionsState != null) {
-      // 맵을 실제적으로 생성함
-      setMap(
-        new kakao.maps.Map(
-          containerState as HTMLDivElement,
-          optionsState as kakao.maps.MapOptions
-        )
-      ); //지도 생성 및 객체 리턴
-    }
-  }, [containerState, optionsState]);
+    setMap(
+      new kakao.maps.Map(
+        container as HTMLDivElement,
+        option as kakao.maps.MapOptions
+      )
+    ); //지도 생성 및 객체 리턴
+  }, []);
 
   // 위도 경도 중심으로 지도 중앙값을 변경해줌.
   useEffect(() => {
-    const moveLatLon = new kakao.maps.LatLng(props.latitude, props.longitude);
+    const moveLatLon = new kakao.maps.LatLng(latitude, longitude);
 
     // 지도 중심을 이동 시킵니다
     map?.panTo(moveLatLon);
-  }, [props.longitude, props.latitude, map]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [longitude, latitude]);
 
   return (
     <>
--- src/components/options/OptionSearch.tsx
+++ src/components/options/OptionSearch.tsx
@@ -1,38 +1,32 @@
-import { useState } from "react";
 import { useOptionStore } from "../../store/optionStore";
 
 export default function OptionSearch() {
-  const [firstBtnClassName, setFirstBtnClassName] = useState(
-    "text-sm mr-2 py-1 px-2 rounded-[3px] bg-white shadow"
-  );
-  const [secondBtnClassName, setSecondBtnClassName] = useState(
-    "text-sm mr-2 py-1 px-2 rounded-[3px]"
-  );
-
-  const { setRank } = useOptionStore();
+  const { rank, setRank } = useOptionStore();
 
   const onClickRankBtn = (value: number) => {
     setRank(value);
-
-    const className = "text-sm mr-2 py-1 px-2 rounded-[3px]";
-    let selected1 = "";
-    let selected2 = "";
-
-    if (value === 1) {
-      selected1 += " bg-white shadow";
-    } else {
-      selected2 += " bg-white shadow";
-    }
-    setFirstBtnClassName(className + selected1);
-    setSecondBtnClassName(className + selected2);
   };
 
   return (
     <div className="z-10 absolute top-0 right-0 m-10 bg-[#f5f5f5] px-1 py-1 text-black rounded-[3px] border-solid border border-[#9ca3af]">
-      <button onClick={() => onClickRankBtn(1)} className={firstBtnClassName}>
+      <button
+        onClick={() => onClickRankBtn(1)}
+        className={
+          rank === 1
+            ? "text-sm py-1 px-2 rounded-[3px] bg-white shadow"
+            : "text-sm py-1 px-2 rounded-[3px]"
+        }
+      >
         1순위
       </button>
-      <button onClick={() => onClickRankBtn(2)} className={secondBtnClassName}>
+      <button
+        onClick={() => onClickRankBtn(2)}
+        className={
+          rank === 2
+            ? "text-sm py-1 px-2 rounded-[3px] bg-white shadow"
+            : "text-sm py-1 px-2 rounded-[3px]"
+        }
+      >
         2-3순위
       </button>
     </div>
