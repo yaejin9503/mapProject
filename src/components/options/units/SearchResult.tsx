5421c1e3ec2e5a8dbb72f9d8f81bd7aea81c77f7
yejin <jimmy0152@naver.com>
2024-03-08 14:14:19 +0900

    Refactor: 조건문 정리

--- src/components/options/units/SearchResult.tsx
+++ src/components/options/units/SearchResult.tsx
@@ -23,9 +23,19 @@ export default function SearchResult(props: IpropsSearchResult) {
     }
   };
 
-  if (data && Array.isArray(data) && data.length > 0) {
-    return (
-      data && (
+  /**
+   * 1. 쿼리가 없음: 쿼리가 없을 때는 아예 이 컴포넌트를 렌더할 필요성이없다.
+   * 2. 쿼리에 해당하는 검색결과가 없음: 검색결과 없음.
+   * 3. 검색결과 있음: 리스트를 리턴
+   */
+
+  if (!data) {
+    return <></>;
+  }
+
+  if (props.query.length > 0) {
+    if (Array.isArray(data) && data.length > 0) {
+      return (
         <div className="absolute mt-1 bg-white w-80 border border-[#9ca3af] rounded text-sm overflow-y-auto max-h-52">
           <div style={{ opacity: props.isStale ? 0.5 : 1 }}>
             <ul>
@@ -42,10 +52,8 @@ export default function SearchResult(props: IpropsSearchResult) {
             </ul>
           </div>
         </div>
-      )
-    );
-  } else {
-    if (props.query.length > 0)
+      );
+    } else {
       return (
         <div className="absolute mt-1 bg-white w-80 border border-[#9ca3af] rounded text-sm overflow-y-auto max-h-52">
           <div style={{ opacity: props.isStale ? 0.5 : 1 }}>
@@ -53,5 +61,6 @@ export default function SearchResult(props: IpropsSearchResult) {
           </div>
         </div>
       );
+    }
   }
 }
