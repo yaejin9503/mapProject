34773a5639ac1c1fbdbc4e13520929219836850d
yejin <jimmy0152@naver.com>
2024-03-23 13:33:54 +0900

    Dose: type 수정 및 타이틀 수정

--- src/store/mapStore.ts
+++ src/store/mapStore.ts
@@ -4,8 +4,8 @@ interface Store {
   selectedMarkerId: number;
   longitude: number;
   latitude: number;
-  myLongitude: number | null;
-  myLatitude: number | null;
+  myLongitude: number;
+  myLatitude: number;
   setSelectedMarkerId: (item: number) => void;
   setLongLat: (item: LogLat) => void;
   setMyLongLat: (item: LogLat) => void;
@@ -20,8 +20,8 @@ const useUserStore = create<Store>((set) => ({
   selectedMarkerId: 0,
   longitude: 126.93990862062978,
   latitude: 37.56496830314491,
-  myLongitude: null,
-  myLatitude: null,
+  myLongitude: 0,
+  myLatitude: 0,
   setSelectedMarkerId: (item: number) =>
     set(() => ({ selectedMarkerId: item })),
   setLongLat: (item: LogLat) =>
