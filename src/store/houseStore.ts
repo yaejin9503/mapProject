009c32c6e3bbdaed72466c791c600526b01adcb5
yejin <jimmy0152@naver.com>
2024-04-09 04:34:27 +0900

    Feat: 원본데이터를 저장할 store 값 추가

--- src/store/houseStore.ts
+++ src/store/houseStore.ts
@@ -1,21 +1,25 @@
 import { create } from "zustand";
-import { Notification } from "../commons/types/types";
+import { HouseInfo, Notification } from "../commons/types/types";
 
 interface House {
   notificationArrs: Notification[];
+  houseData: HouseInfo[];
   setNotificationArrs: (item: Notification[]) => void;
   getAllNotification: () => void;
   getOneNotification: (typeId: string) => void;
+  setHouseData: (item: HouseInfo[]) => void;
 }
 
 const useHouseStore = create<House>((set, get) => ({
   notificationArrs: [],
+  houseData: [],
   setNotificationArrs: (item: Notification[]) =>
     set(() => ({ notificationArrs: item })),
   getAllNotification: () => get().notificationArrs,
   getOneNotification: (typeId: string) => {
     return get().notificationArrs.find((item) => item.id === typeId);
   },
+  setHouseData: (item: HouseInfo[]) => set(() => ({ houseData: item })),
 }));
 
 export { useHouseStore };
